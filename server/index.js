b05156757cb70c6e42d8f7a9c0d047578b7f51d8
ranjan-dev40 <roy984258@gmail.com>
2023-07-11 02:41:49 +0530

    adding cors policy

--- server/index.js
+++ server/index.js
@@ -4,68 +4,50 @@ const http = require('http');
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 
-const io = socketio(server, {
-  cors: {
-    origin: process.env.USER_URL,
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  },
-});
+
+const io = socketio(server);
+
+app.use(cors());
+app.use(router);
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) {
-      return callback && callback(error); // Return error through the callback if provided
-    }
+    if(error) return callback(error);
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to  ${user.room} room.` });
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
-    if (callback && typeof callback === 'function') {
-      callback(); // Call the callback to acknowledge successful join if provided
-    }
+    callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
-    if (!user) {
-      // Handle case where user is not found
-      return callback && callback('User not found');
-    }
-
     io.to(user.room).emit('message', { user: user.name, text: message });
 
-    if (callback && typeof callback === 'function') {
-      callback(); // Call the callback to acknowledge successful message sending if provided
-    }
+    callback();
   });
 
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
 
-    if (user) {
-      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
     }
-  });
+  })
 });
 
-// app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(cors({ origin: process.env.USER_URL }));
-app.use(router);
-
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
