69b1cddfa6750abfb92143cf47b84d8ce26fdaf4
ranjan-dev40 <roy984258@gmail.com>
2023-07-11 04:29:50 +0530

    gfds

--- server/index.js
+++ server/index.js
@@ -22,6 +22,17 @@ const io = socketio(server, {
 
 
 // app.use(cors({ origin: 'http://localhost:3000' }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    
+    next();
+
+});
 app.use(cors({ origin: 'https://unique-halva-6abbeb.netlify.app' }));
 
 app.use(router);
